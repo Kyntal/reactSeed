5c947c58fb1b07b333411a9d8fe315875bf9d9f6
Claudio <clandres1985@gmail.com>
2022-02-15 18:30:00 -0300

    3rd Section Finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import Cabecera  from './components/Cabecera';
+import P from './components/P';
 import './App.css';
 
 class App extends Component {
+  state = {
+    miau: 'Bienvenido a miau'
+  }
+  cambiarTextoDelEstado = () => {
+    this.setState({ miau: 'Hola Mundo'})
+  }
   manejaClick = texto => {
     console.log(texto)
   }
   render() {
-    const miau = "Bienvenido miau"
+    const { miau } = this.state
     return (
       <div className="App">
         <Cabecera miau={miau} manejaClick={this.manejaClick}/>
-        <p className='App-intro'>
-          Hola mundo
-        </p>
+        <P onClick={this.cambiarTextoDelEstado}>
+          {miau}
+        </P>
       </div>
     );
   }
