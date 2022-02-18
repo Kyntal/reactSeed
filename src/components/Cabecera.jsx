431f7474c7797dd12f647237a300f93e2b746c9f
Claudio <clandres1985@gmail.com>
2022-02-18 11:27:26 -0300

    4 Finish

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
 import logo from '../logo.svg';
+import H1 from "./H1";
+
+const styles = {
+    header: ({ backgroundColor }) => ({
+        backgroundColor,
+        height: '150px',
+        padding: '20px',
+        color: 'white',
+    })
+}
 
 export default class Cabecera extends Component {
+    state = {
+        backgroundColor: '#222',
+    }
+
+    cambiaColorHeader = () => {
+        this.setState({cambiaColorHeade: '#FF0000'})
+    }
+
     manejaClick = () => {
         const { manejaClick, miau } = this.props
         manejaClick(miau)
     }
+
     render() {
         const { miau, manejaClick } = this.props
+        const { backgroundColor } = this.state
 
         return (
-            <header className="App-header">
+            <header onClick={this.cambiaColorHeader} style={styles.header({ backgroundColor })}>
                 <img onClick={this.manejaClick} src={logo} className="App-logo" alt="logo" />
-                <h1 className='App-title'>{miau}</h1>
+                <H1>{miau}</H1>
             </header>
         )
     }
