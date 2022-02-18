10758c008f3fe4d5ea5f7f7fed303a01c8113c43
Claudio <clandres1985@gmail.com>
2022-02-18 12:25:01 -0300

    Manejo de Formularios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
-import Cabecera  from './components/Cabecera';
-import P from './components/P';
-import './App.css';
+import FormularioSimple from './components/FormularioSimple'
 
 class App extends Component {
-  state = {
-    miau: 'Bienvenido a miau'
-  }
-  cambiarTextoDelEstado = () => {
-    this.setState({ miau: 'Hola Mundo'})
-  }
-  manejaClick = texto => {
-    console.log(texto)
-  }
   render() {
-    const { miau } = this.state
     return (
       <div className="App">
-        <Cabecera miau={miau} manejaClick={this.manejaClick}/>
-        <P onClick={this.cambiarTextoDelEstado}>
-          {miau}
-        </P>
+        <FormularioSimple />
       </div>
     );
   }
